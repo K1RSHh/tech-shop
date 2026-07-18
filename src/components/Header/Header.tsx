5878a960bbdb55b191116f445467ed0eb6c26846
Kirya <mark2jzx100ua@gmail.com>
2026-07-18 22:03:07 +0300

    header update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,11 @@
 import { Link } from "react-router-dom";
-import { ChevronDown } from "lucide-react";
+import {
+  ChevronDown,
+  TextAlignJustify,
+  Search,
+  ShoppingCart,
+  CircleUserRound,
+} from "lucide-react";
 
 function Header() {
   return (
@@ -28,7 +34,32 @@ function Header() {
           </div>
         </div>
       </div>
-      <div></div>
+      {/*header bottom*/}
+      <div className="flex justify-between items-center bg-blue-600 w-full px-4 h-16">
+        <div className=" flex items-center">
+          <button>
+            <TextAlignJustify size={28} color="#fff" />
+          </button>
+        </div>
+        <div className="flex items-center p-3 max-h-10 max-w-65 md:w-lg md:max-w-lg bg-white gap-2 rounded-3xl">
+          <Search size={25} color="#CACDD8" />
+          <input
+            type="text"
+            placeholder="Search here"
+            className="placeholder-gray-300 max-w-50 md:max-w-md md:w-100 font-normal text-black focus:outline-none"
+          />
+        </div>
+        <div>
+          <button>
+            <ShoppingCart size={28} />
+          </button>
+        </div>
+        <div>
+          <button>
+            <CircleUserRound size={28} />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
